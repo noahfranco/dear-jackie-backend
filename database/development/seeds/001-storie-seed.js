f868a308a8c41f3cadd46141a20828d98e6d7308
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-02-03 20:47:30 -0500

    working on debugging error

--- database/development/seeds/001-storie-seed.js
+++ database/development/seeds/001-storie-seed.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('storie-seed').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('storie-seed').insert([
         {id: 1, title: 'Seed Data Test', date: '02-2-20', description: 'Testing to see if seed data is working', longitude: 47.620422, latitude: -122.349358},
         {id: 2, colName: 'rowValue2',},
         {id: 3, colName: 'rowValue3', }
