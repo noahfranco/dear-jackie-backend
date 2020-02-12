405c8b93a50478f4c04f393d90fbdf861d8c963c
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-02-12 09:55:39 -0500

    changed name to seed data

--- database/development/seeds/001-seed-data.js
+++ database/development/seeds/001-seed-data.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('seed-data').del()
+  return knex('001-seed-data').del()
     .then(function () {
       // Inserts seed entries
-      return knex('seed-data').insert([
+      return knex('001-seed-data').insert([
         {id: 1, title: 'Seed Data Test', date: '02-2-20', description: 'Testing to see if seed data is working', longitude: 47.620422, latitude: -122.349358},
       ]);
     });
