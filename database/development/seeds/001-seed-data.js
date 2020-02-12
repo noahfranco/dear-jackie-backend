3511ebcd38f8decab007e171a749023f1bb2e32d
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-02-12 09:50:35 -0500

    added new seed-data

--- database/development/seeds/001-seed-data.js
+++ database/development/seeds/001-seed-data.js
@@ -1,13 +1,11 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('seed-data').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('seed-data').insert([
+        {id: 1, title: 'Seed Data Test', date: '02-2-20', description: 'Testing to see if seed data is working', longitude: 47.620422, latitude: -122.349358},
       ]);
     });
 };
